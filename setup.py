901a81fcbed14259567b00391827a8e969b2ba59
mitosagi <54105954+mitosagi@users.noreply.github.com>
2021-08-08 21:47:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup, Extension
+# [Why is python setup.py saying invalid command 'bdist_wheel' on Travis CI? - Stack Overflow](https://stackoverflow.com/questions/34819221/why-is-python-setup-py-saying-invalid-command-bdist-wheel-on-travis-ci/50314071#50314071)
+import setuptools
 
 module = Extension('puzzpy',
                     include_dirs=['extern/pybind11/include'],
